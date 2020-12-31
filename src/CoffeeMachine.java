1edc66411390031c60bd6fa0032ff4bc02f4766c
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-12-31 21:34:42 +0530

    Complete Stage 3/6: Estimate the number of servings

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -3,15 +3,39 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int water = 200, milk = 50, coffee = 15;
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int waterAvail = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milkAvail = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeAvail = scanner.nextInt();
+
+        int waterCup =  waterAvail / water;
+        int milkCup = milkAvail / milk;
+        int coffeeCup = coffeeAvail / coffee;
+
+        int minCups = waterCup < milkCup ? (waterCup < coffeeCup ? waterCup : coffeeCup ) : ( milkCup < coffeeCup ? milkCup : coffeeCup );
+
         System.out.println("Write how many cups of coffee you will need:");
         int cups = scanner.nextInt();
-        water *= cups;
-        milk *= cups;
-        coffee *= cups;
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffee + " g of coffee beans");
+
+        if(cups == minCups){
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+        else if (cups < minCups){
+            System.out.println("Yes, I can make that amount of coffee (and even " + (minCups - cups) + " more than that)");
+        } else{
+            System.out.println("No, I can make only " + minCups + " cup(s) of coffee");
+        }
+
+//        System.out.println(water + " ml of water");
+//        System.out.println(milk + " ml of milk");
+//        System.out.println(coffee + " g of coffee beans");
 
 //        System.out.println("Starting to make a coffee");
 //        System.out.println("Grinding coffee beans");
